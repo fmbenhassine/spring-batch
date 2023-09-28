a80629fcf10566f42bf53a471821e44105977421
Mahmoud Ben Hassine <mbenhassine@vmware.com>
2023-09-28 16:19:03 +0200

    Fix code formatting

--- spring-batch-core/src/main/java/org/springframework/batch/core/explore/support/MongoJobExplorerFactoryBean.java
+++ spring-batch-core/src/main/java/org/springframework/batch/core/explore/support/MongoJobExplorerFactoryBean.java
@@ -14,35 +14,36 @@ import org.springframework.util.Assert;
 
 public class MongoJobExplorerFactoryBean extends AbstractJobExplorerFactoryBean implements InitializingBean {
 
-    private MongoOperations mongoOperations;
+	private MongoOperations mongoOperations;
 
-    public void setMongoOperations(MongoOperations mongoOperations) {
-        this.mongoOperations = mongoOperations;
-    }
+	public void setMongoOperations(MongoOperations mongoOperations) {
+		this.mongoOperations = mongoOperations;
+	}
 
-    @Override
-    protected JobInstanceDao createJobInstanceDao() {
-        return new MongoJobInstanceDao(this.mongoOperations);
-    }
+	@Override
+	protected JobInstanceDao createJobInstanceDao() {
+		return new MongoJobInstanceDao(this.mongoOperations);
+	}
 
-    @Override
-    protected JobExecutionDao createJobExecutionDao() {
-        return new MongoJobExecutionDao(this.mongoOperations);
-    }
+	@Override
+	protected JobExecutionDao createJobExecutionDao() {
+		return new MongoJobExecutionDao(this.mongoOperations);
+	}
 
-    @Override
-    protected StepExecutionDao createStepExecutionDao() {
-        return new MongoStepExecutionDao(this.mongoOperations);
-    }
+	@Override
+	protected StepExecutionDao createStepExecutionDao() {
+		return new MongoStepExecutionDao(this.mongoOperations);
+	}
 
-    @Override
-    protected ExecutionContextDao createExecutionContextDao() {
-        return new MongoExecutionContextDao(this.mongoOperations);
-    }
+	@Override
+	protected ExecutionContextDao createExecutionContextDao() {
+		return new MongoExecutionContextDao(this.mongoOperations);
+	}
 
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        super.afterPropertiesSet();
-        Assert.notNull(this.mongoOperations, "MongoOperations must not be null.");
-    }
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		super.afterPropertiesSet();
+		Assert.notNull(this.mongoOperations, "MongoOperations must not be null.");
+	}
+
 }
